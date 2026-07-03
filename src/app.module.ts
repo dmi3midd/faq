9b7187340864591de1835f90622ec36e550158bc
dmi3midd <dmi3midd@gmail.com>
2026-07-03 12:06:17 +0300

    Init commit.

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.getOrThrow<string>("POSTGRES_HOST"),
+        port: configService.getOrThrow<number>("POSTGRES_PORT"),
+        username: configService.getOrThrow<string>("POSTGRES_USERNAME"),
+        password: configService.getOrThrow<string>("POSTGRES_PASSWORD"),
+        database: configService.getOrThrow<string>("POSTGRES_DATABASE"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
