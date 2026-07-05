be66b5a24ae818888e83d55b9d598f034f574e8a
dmi3midd <dmi3midd@gmail.com>
2026-07-05 16:08:51 +0300

    refactor: remove article module.

--- src/article/article.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ArticleService } from './article.service';
-
-@Controller('article')
-export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
-}
